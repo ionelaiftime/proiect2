3d2eeecd4c28da32ea41fd131a8998b98ab5bd71
Ionela Iftime <ionela-alexandra.iftime@student.tuiasi.ro>
2020-05-20 09:59:05 +0300

    lab12.2.d

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
 
 const app = express();
+
 const fs = require('fs');
 var listaUtilizatori=[];
 fs.readFile('utilizatori.json', (err, data) => {
@@ -28,11 +29,11 @@ app.use(session({
   }
 }));
 
+app.use("/public",express.static("public"));
 session.utilizator=null;
+session.cos=[]
 
 // directorul 'views' va conține fișierele .ejs (html + js executat la server)
-
-
 app.set('view engine', 'ejs');
 // suport pentru layout-uri - implicit fișierul care reprezintă template-ul site-ului este views/layout.ejs
 app.use(expressLayouts);
@@ -45,9 +46,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.set('trust proxy', 1) 
-
-
-
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -57,8 +55,6 @@ var con = mysql.createConnection({
   database:"perifericepc"
 });
 
-
-// la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
 app.get('/', (req, res) =>
@@ -78,41 +74,50 @@ app.get('/', (req, res) =>
     res.render('index',{result,utilizator:session.utilizator});
   });
    
-}
-);
+});
 
 app.get('/index',(req,res) =>
 {
-
-    res.render('index',{utilizator:session.utilizator});
-
+  con.query("select * from produse",function(err,result,fields)
+  {
+    if(err) throw err;
+   // console.log(result);
+    
+    result.forEach( (result) => {
+     // console.log(`${result.nume} price ${result.pret}`);
+      //res.send(result);
+     
+    });
   
+    res.render('index',{result,utilizator:session.utilizator});
+  });
+
 });
+
+
 app.get('/logout',(req,res) =>
 {
   session.utilizator=null;
+  session.cos=[];
   res.redirect('/');
 });
 
 
-app.get('/autentificare', (req, res) => 
-
-{
-  res.clearCookie('mesajEroare'); 
-  res.render('autentificare',{mesajEroare:req.cookies["mesajEroare"]});
-  
-});
-
 app.get('/creare-bd', (req, res) => 
 {
   
-    console.log("Connected!");
-    var sql = "CREATE Schema IF NOT EXISTS perifericepc";
+  
+    //console.log("Connected!");
+    var sql = "CREATE Schema IF NOT EXISTS cumparaturi";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("Database created");
     });
-    var sql = "CREATE TABLE IF NOT EXISTS perifericepc.produse(nume varchar(20),pret decimal(10));";
+    con.changeUser({ database: "cumparaturi" }, function (err) {
+      if (err) throw err;
+      console.log("Connected cumparaturi!");
+    }); 
+    var sql = "CREATE TABLE IF NOT EXISTS produse( id INT NOT NULL AUTO_INCREMENT primary key NOT NULL,nume varchar(20)NOT NULL,pret decimal(10));";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("Table created");
@@ -139,22 +144,18 @@ app.get('/inserare-bd', (req, res) =>
 
 //app.get('/autentificare', (req, res) => res.render('autentificare'));
 
-
-app.get('/chestionar', (req, res) => {
-
-  fs.readFile('intrebari.json', (err, data) => {
-    //if (err) throw err;
-    listaIntrebari = JSON.parse(data);
-    //console.log(listaIntrebari);
-    res.render('chestionar', {intrebari: listaIntrebari,utilizator:session.utilizator});
+app.get('/autentificare', (req, res) => 
+{
+  res.clearCookie('mesajEroare'); 
+  res.render('autentificare',{mesajEroare:req.cookies["mesajEroare"]});
+  
 });
 
-	// în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebărirtrhttg
-});
 
 
 app.post('/verificare-autentificare', (req, res) => {
     var corect=false;
+    //console.log(req.body);
     for(let i=0;i<listaUtilizatori.length;i++)
     {
   
@@ -180,7 +181,17 @@ app.post('/verificare-autentificare', (req, res) => {
     });
   
 
+app.get('/chestionar', (req, res) => {
 
+  fs.readFile('intrebari.json', (err, data) => {
+    //if (err) throw err;
+    listaIntrebari = JSON.parse(data);
+    //console.log(listaIntrebari);
+    res.render('chestionar', {intrebari: listaIntrebari,utilizator:session.utilizator});
+  });
+    
+  // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebărirtrhttg
+});
 
 app.post('/rezultat-chestionar', (req, res) => {
   let json=JSON.stringify(req.body);
@@ -200,6 +211,32 @@ app.post('/rezultat-chestionar', (req, res) => {
   res.render('rezultat-chestionar',{intrebari: listaIntrebari,Raspunsuri_corecte:b,utilizator:session.utilizator});
 });
  
+app.post('/adaugare_cos', (req, res) => 
+{
+  var it=req.body.id;
+  //console.log(it);
+  if(req.session.cos===undefined){
+      req.session.cos=[];}
+  req.session.cos.push(it);
+  
+  console.log(req.session.cos);    
+  res.redirect("/");
 
+  
+});
+
+app.get('/vizualizare-cos',(req,res) =>{
+
+  var a=[];
+  for(let i=0;i<session.cos.length;i++)
+  {
+    a.push(session.cos[i].id);
+
+  }
+  console.log(a);
+  res.render('vizualizare-cos',{utilizator:session.utilizator});
+
+
+});
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
